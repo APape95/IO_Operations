506e2e1ebdec96e76a24c509be20ba1e1a777428
Amy Pape <A-C.1995@outlook.com>
2024-02-20 16:39:33 +0000

    correcting errors on this code
    For the dob_task.py task, you should use the close() method with parentheses, and you need to adjust the printing logic to display the names and birthdates correctly. If you want to go further, you might want to handle cases where the names have different lengths. Use the 'with' statement for file handling: The with statement automatically takes care of closing the file, even if an exception occurs.

--- dob_task.py
+++ dob_task.py
@@ -13,22 +13,21 @@
 # ect...
 
 # Open the file.
-file1 = open("DOB.txt")
+with open("DOB.txt") as file1:
 # Printing the heaading for the first section of data.
-print("Name")
+    print("Name")
 # Loop for as many lines in the data and split then into words.
 # Print the first two words as these are the first and last names.
-for line in file1:
-    words = line.split()
-    print(words[0:2])
+    for line in file1:
+        words = line.split()
+        print(words[0], words[1])
 # Print a blank space to leave a line before next set of data.
 print("")
-# Closing the file once first data taken.
-file1.close
 
 # Open the file this time printing after 2nd word to end to show the DOB
-file1 = open("DOB.txt")
-print("Birthdates")
-for line in file1:
-    words = line.split()    
-    print(words[2:])
+with open("DOB.txt") as file1:
+    # Printing the heading for the second section
+    print("Birthdates")
+    for line in file1:
+        words = line.split()    
+        print(" ".join(words[2:]))
